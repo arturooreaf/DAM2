dd623fecc64c0ef8daac289450232f3220fce3aa
aoreaf-alt <aoreaf@gmail.com>
2026-09-20 11:41:24 +0200

    Remove invalid PHP tag inside HTML comment

--- AD/curso-php/islas.php
+++ AD/curso-php/islas.php
@@ -7,7 +7,6 @@
 <body>
   <h1>Islas de PHP</h1>
 
-  <!-- Todo lo que esta fuera de <?php ... ?> se manda al navegador tal cual. -->
   <ul>
     <?php for ($i = 1; $i <= 3; $i++): ?>
       <li>Elemento numero <?php echo $i; ?></li>
