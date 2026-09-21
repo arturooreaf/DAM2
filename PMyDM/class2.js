5e137c16eefc02950f97dc725e264ddeb7444748
aoreaf-alt <aoreaf@gmail.com>
2026-09-21 20:40:45 +0200

    Cambios en los atributos

--- PMyDM/class2.js
+++ PMyDM/class2.js
@@ -6,3 +6,16 @@ console.log("Eres mayor de edad")
  else if(edad>65) {
 console.log("Eres un anciano")
  } else console.log("Es menor de edad")
+
+
+ let edad1 = 18;
+ if (edad<18){
+    console.log("Eres menor de edad")
+ }else if (edad <64){
+    console.log("eres adulto")
+ } else{
+    console.log("Eres mayor de edad");
+ }
+
+
+ 
