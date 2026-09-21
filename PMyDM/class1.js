ce8d5cefccf5a5a5d70676950f799ba2a2de0699
aoreaf-alt <aoreaf@gmail.com>
2026-09-21 16:23:18 +0200

    Ejercicios en JavaScript sobre los if y los else

---
+++ PMyDM/class1.js
@@ -0,0 +1,26 @@
+let n = 32;
+if(n % 2 === 0){
+    console.log("PAR")
+}
+else {
+    console.log("IMPAR")
+}
+
+
+
+let n1 = 4;
+let resultado = n1 % 2 ? "IMPAR" : "PAR"
+console.log(resultado)
+
+
+n = 31;
+let s = "";
+if(n % 2 === 0)
+{
+    s ="IM";
+}
+console.log(s+"PAR")
+
+
+
+
---
+++ PMyDM/class2.js
@@ -0,0 +1,8 @@
+let edad = 16
+if(edad>18) 
+    {
+console.log("Eres mayor de edad")
+    }
+ else if(edad>65) {
+console.log("Eres un anciano")
+ } else console.log("Es menor de edad")
