e70e60106a56e5ab5680ec700bd7bf6dd8ef2ac0
aoreaf-alt <aoreaf@gmail.com>
2026-09-22 17:30:39 +0200

    Clase 22-09-2026

--- AD/curso-php/index.php
+++ AD/curso-php/index.php
@@ -8,6 +8,64 @@
   <h1>
     <?php
       echo "Hola mundo desde PHP";
+      $edad = 24;
+      $nombre = 45;
+      $apellido = "fernandez";
+
+      echo $edad . $nombre . $apellido ;
+
+echo "</br>";
+
+
+
+for($i = 1;  $i< 7; $i++){ 
+
+
+      if($i>=6) {  
+        echo  " ". $i . " " ;
+      }
+       else {
+                echo " ".  $i . ","; 
+      }
+}
+
+echo "</br>";
+
+$tamano  = 7;
+for($i = 1 ; $i<= $tamano; $i++){
+  echo $i;
+  if($i< $tamano){
+    echo ", ";
+  }
+}
+
+
+echo "</br>";
+
+$comma = "";
+for($i=1; $i<=$tamano; $i++)
+  {
+     echo $comma. $i;
+    $comma = ",";
+  }
+   
+
+echo "</br>";
+
+$edad = "ocho";
+$edad = "nueve";
+
+var_export($edad);
+echo "</br>";
+echo gettype($edad);
+ 
+echo "</br>";
+
+echo 'el valor de edad es $edad ';
+echo "el valor de la edad es $edad";
+
+
+define('PI',3,141592)
     ?>
   </h1>
 </body>
---
+++ AD/curso-php/index2.php
@@ -0,0 +1,11 @@
+<?php
+
+function saludar ($nombre, $saludo){
+
+    return $saludo .  $nombre;
+
+}
+
+echo saludar(" Bego", " Que tal")
+
+?>
